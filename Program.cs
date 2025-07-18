28d22c52b492725472d62b830abda645fec3e68a
Eddie Mauricio <huedoctor35@gmail.com>
2025-07-18 11:20:42 -0300

    propriedades criadas, construtores feitos, classes herdadas, e metodos sobrescritos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("1234567", "Modelo 1", "1111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Iphone("09809", "Modelo 2", "22222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
